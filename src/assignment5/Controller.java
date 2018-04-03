3b3234b87c0ef01352e55d0add746f2fe17b318e
Unknown <zack.c.campbell@utexas.edu>
2018-04-02 19:26:36 -0500

    creating layout of controller

--- src/assignment5/Controller.java
+++ src/assignment5/Controller.java
@@ -3,7 +3,29 @@ package assignment5;
 import javafx.event.ActionEvent;
 
 public class Controller {
+    private int quantityMake = 0;
+    private int width = 0;
+    private int height = 0;
+
+    // Not sure what to put here just yet
+    public void makeWorld(ActionEvent ae) {
+        Critter.displayWorld();
+    }
+
     public void testButton(ActionEvent ae) {
         System.out.println("test");
     }
+
+    public void stepAction(ActionEvent ae) {
+        Critter.worldTimeStep();
+    }
+
+    public void makeAction(ActionEvent ae) {
+
+    }
+
+    public void getCritters(ActionEvent ae) {
+
+    }
+
 }
